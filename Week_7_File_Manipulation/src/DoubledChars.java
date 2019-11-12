e6b96717db1c8fafcbab807112b0e23594846896
NemethJohanna <nemethkjohanna@gmail.com>
2019-11-12 20:42:41 +0100

    doubled-char: creat a file and read content

--- Week_7_File_Manipulation/src/DoubledChars.java
+++ Week_7_File_Manipulation/src/DoubledChars.java
@@ -1,32 +1,32 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class DoubledChars {
-        public static void main(String[] args) {
+        public static void main(String[] args) throws IOException {
             // Create a method that decrypts the duplicated-chars.txt
 
-//            TThhee  ZZeenn  ooff  PPyytthhoonn
-//            bbyy  TTiimm  PPeetteerrss
-//
-//            BBeeaauuttiiffuull  iiss  bbeetttteerr  tthhaann  uuggllyy..
-//            EExxpplliicciitt  iiss  bbeetttteerr  tthhaann  iimmpplliicciitt..
-//            SSiimmppllee  iiss  bbeetttteerr  tthhaann  ccoommpplleexx..
-//            CCoommpplleexx  iiss  bbeetttteerr  tthhaann  ccoommpplliiccaatteedd..
-//            FFllaatt  iiss  bbeetttteerr  tthhaann  nneesstteedd..
-//            SSppaarrssee  iiss  bbeetttteerr  tthhaann  ddeennssee..
-//            RReeaaddaabbiilliittyy  ccoouunnttss..
-//            SSppeecciiaall  ccaasseess  aarreenn''tt  ssppeecciiaall  eennoouugghh  ttoo  bbrreeaakk  tthhee  rruulleess..
-//            AAlltthhoouugghh  pprraaccttiiccaalliittyy  bbeeaattss  ppuurriittyy..
-//            EErrrroorrss  sshhoouulldd  nneevveerr  ppaassss  ssiilleennttllyy..
-//            UUnnlleessss  eexxpplliicciittllyy  ssiilleenncceedd..
-//            IInn  tthhee  ffaaccee  ooff  aammbbiigguuiittyy,,  rreeffuussee  tthhee  tteemmppttaattiioonn  ttoo  gguueessss..
-//            TThheerree  sshhoouulldd  bbee  oonnee----  aanndd  pprreeffeerraabbllyy  oonnllyy  oonnee  ----oobbvviioouuss  wwaayy  ttoo  ddoo  iitt..
-//            AAlltthhoouugghh  tthhaatt  wwaayy  mmaayy  nnoott  bbee  oobbvviioouuss  aatt  ffiirrsstt  uunnlleessss  yyoouu''rree  DDuuttcchh..
-//            NNooww  iiss  bbeetttteerr  tthhaann  nneevveerr..
-//            AAlltthhoouugghh  nneevveerr  iiss  oofftteenn  bbeetttteerr  tthhaann  **rriigghhtt**  nnooww..
-//            IIff  tthhee  iimmpplleemmeennttaattiioonn  iiss  hhaarrdd  ttoo  eexxppllaaiinn,,  iitt''ss  aa  bbaadd  iiddeeaa..
-//            IIff  tthhee  iimmpplleemmeennttaattiioonn  iiss  eeaassyy  ttoo  eexxppllaaiinn,,  iitt  mmaayy  bbee  aa  ggoooodd  iiddeeaa..
-//            NNaammeessppaacceess  aarree  oonnee  hhoonnkkiinngg  ggrreeaatt  iiddeeaa  ----  lleett''ss  ddoo  mmoorree  ooff  tthhoossee!!
+            File file = new File("duplicated-chars.txt");
+            while (file.exists() != true) {
+                 file.createNewFile();
+            }
 
 
-
-
+            Path path = Paths.get("duplicated-chars.txt");
+            try {
+                List<String> lines = Files.readAllLines(path);
+                for (String line : Files.readAllLines(path)) {
+                    System.out.println(line);
+                }
+            } catch (IOException ex) {
+                ex.getStackTrace();
+            }
         }
 }
