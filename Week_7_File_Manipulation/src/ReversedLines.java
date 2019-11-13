10cce05b673ef5b3f282dc3025eb66f237a16170
NemethJohanna <nemethkjohanna@gmail.com>
2019-11-13 12:28:02 +0100

    finished

---
+++ Week_7_File_Manipulation/src/ReversedLines.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReversedLines {
+
+    public static void main(String[] args) throws IOException {
+        // Create a method that decrypts reversed-lines.txt
+
+        Path filePath = Paths.get("reversed-lines.txt");
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(filePath);
+            reversedLines(lines);
+        } catch (IOException e) {
+            System.out.println("Unable to read file: reversed-lines.txt");
+            e.printStackTrace();
+        }
+    }
+    public static void reversedLines (List<String> lineList) {
+            for (int i = 0; i < lineList.size(); i++) {
+                for (int j = lineList.get(i).length() - 1; j >= 0; j--) {
+                    System.out.print(lineList.get(i).charAt(j));
+                }
+                System.out.println();
+            }
+
+    }
+}
---
+++ Week_7_File_Manipulation/src/ReversedOrder.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReversedOrder {
+    // Create a method that decrypts reversed-order.txt
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("reversed-order.txt");
+        List<String> order = new ArrayList();
+        try {
+            order = Files.readAllLines(path);
+            reverseOrder(order);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void reverseOrder (List<String> orderList) {
+        for (int i = orderList.size() - 1; i >= 0; i--) {
+            System.out.println(orderList.get(i));
+        }
+        System.out.println();
+    }
+}
