b00acf9c660c5548f42488638b614366b4003352
NemethJohanna <nemethkjohanna@gmail.com>
2019-11-18 23:12:14 +0100

    Finished pokemon

--- Week_8_OOP/src/O04_Sharpie/Sharpie.java
+++ Week_8_OOP/src/O04_Sharpie/Sharpie.java
@@ -5,7 +5,7 @@ public class Sharpie {
 We should know about each sharpie their
  - color (which should be a string),
  - width (which will be a floating point number),
-inkAmount (another floating point number)
+ - inkAmount (another floating point number)
 
 When creating one, we need to specify the color and the width
 Every sharpie is created with a default 100 as inkAmount
--- Week_8_OOP/src/O06_Pokemon/Main.java
+++ Week_8_OOP/src/O06_Pokemon/Main.java
@@ -1,5 +1,6 @@
 package O06_Pokemon;
 
+import javax.naming.Name;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,8 @@ public class Main {
         List<Pokemon> pokemonOfAsh = initializePokemons();
 
         // Every pokemon has a name and a type.
-        // Certain types are effective against others, e.g. water is effective against fire.
+        // Certain types are effective against others,
+        // e.g. water is effective against fire.
 
         // Ash has a few pokemon.
         // A wild pokemon appeared!
@@ -17,8 +19,21 @@ public class Main {
 
         // Which pokemon should Ash use?
 
-        System.out.print("I choose you, ");
+        // I create a for loop to find out that pokemon,
+        // which is effective against LEAF. Correct answer is: "Charizard".
+
+        for (int i = 0; i < pokemonOfAsh.size(); i++) {
+            if (pokemonOfAsh.get(i).effectiveAgainst.equals(wildPokemon.type)) {
+                System.out.print("I choose you, " + pokemonOfAsh.get(i).name);
+            }
+        }
+
     }
+
+    // I create a public String getName object to get the private object's name.
+//    public String getName (){
+//        return name;
+//    }
 
     private static List<Pokemon> initializePokemons() {
         List<Pokemon> pokemon = new ArrayList<>();
