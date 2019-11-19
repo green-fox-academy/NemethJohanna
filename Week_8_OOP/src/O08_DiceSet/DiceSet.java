adbfaf4f61f9ed1c68bbfa71ae2dfa3dcbf7a913
NemethJohanna <nemethkjohanna@gmail.com>
2019-11-19 10:29:15 +0100

    Finished Dice

--- Week_8_OOP/src/O08_DiceSet/DiceSet.java
+++ Week_8_OOP/src/O08_DiceSet/DiceSet.java
@@ -21,31 +21,15 @@ public class DiceSet {
         return dice.get(i);
     }
 
-    public void reroll() {
+    public void reRoll() {
         for (int i = 0; i < dice.size(); i++) {
             dice.set(i, (int) (Math.random() * 6) + 1);
         }
     }
 
-    public void reroll(int k) {
+    public void reRoll(int k) {
         dice.set(k, (int) (Math.random() * 6) + 1);
     }
 
-    public static void main(String[] args) {
-        // You have a `DiceSet` class which has a list for 6 dice
-        // You can roll all of them with roll()
-        // Check the current rolled numbers with getCurrent()
-        // You can reroll with reroll()
-        // Your task is to roll the dice until all of the dice are 6
 
-        DiceSet diceSet = new DiceSet();
-        System.out.println(diceSet.getCurrent());
-        System.out.println(diceSet.roll());
-        System.out.println(diceSet.getCurrent());
-        System.out.println(diceSet.getCurrent(5));
-        diceSet.reroll();
-        System.out.println(diceSet.getCurrent());
-        diceSet.reroll(4);
-        System.out.println(diceSet.getCurrent());
-    }
 }
