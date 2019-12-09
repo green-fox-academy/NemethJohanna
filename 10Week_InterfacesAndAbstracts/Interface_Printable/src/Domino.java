37690f369645e1664371ea776ff430b8113fe8e0
NemethJohanna <nemethkjohanna@gmail.com>
2019-12-09 12:09:27 +0100

    created the classes of printable excercise

---
+++ 10Week_InterfacesAndAbstracts/Interface_Printable/src/Domino.java
@@ -0,0 +1,10 @@
+public class Domino implements Printable{
+
+    private int aSide;
+    private int bSide;
+
+    @Override
+    public void printAllFields() {
+
+    }
+}
---
+++ 10Week_InterfacesAndAbstracts/Interface_Printable/src/Todo.java
@@ -0,0 +1,12 @@
+public class Todo implements Printable{
+
+    private String task;
+    private String priority;
+    private boolean isDone;
+
+
+    @Override
+    public void printAllFields() {
+
+    }
+}
