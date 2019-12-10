e003ab57a8b421233b09536c9fb1ff5f1c70c071
NemethJohanna <nemethkjohanna@gmail.com>
2019-12-10 09:29:34 +0100

    reservation classes created

---
+++ 10Week_InterfacesAndAbstracts/Interface_Reservation/src/Reservation.java
@@ -0,0 +1,20 @@
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reservation implements Reservationy {
+
+    private int numberOfCharacters;
+    
+    @Override
+    public String getDowBooking() {         // Day Of The Week (MON, THU, WED...)
+        List<DayOfWeek> days = new ArrayList<>();
+
+        return days.toString();
+    }
+
+    @Override
+    public String getCodeBooking() {
+        generateCode();
+    }
+}
