8e551aef0963827ad7f1c5c1187afcf16cfb253e
NemethJohanna <nemethkjohanna@gmail.com>
2019-12-16 10:23:48 +0100

    added readme file with link

---
+++ 12Week_ToDoApp/src/ToDoApp.java
@@ -0,0 +1,10 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ToDoApp {
+    public static void main(String[] args) {
+
+        Path path = Paths.get("ToDoList");
+
+    }
+}
