2f5473117973d31e27016cf2071e8b602e945a3e
NemethJohanna <nemethkjohanna@gmail.com>
2019-12-28 10:26:49 +0100

    ready with add method

--- 09Week_Inheritance/src/I03_AircraftCarrier/I03_AircraftCarrier/Carrier.java
+++ 09Week_Inheritance/src/I03_AircraftCarrier/I03_AircraftCarrier/Carrier.java
@@ -1,17 +1,29 @@
 package I03_AircraftCarrier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carrier {
 
-    Aircraft aircraft1 = new Aircraft();
-    Aircraft aircraft2 = new Aircraft();
+    private String name;
+    private List<Aircraft> aircrafts;
+
+    public Carrier(String name) {
+        this.name = name;
+        aircrafts = new ArrayList<>();
+    }
 
     //It should take a new aircraft and add it to its storage
-    public void add (){    }
+    public void add (Aircraft aircraft){
+        this.aircrafts.add(aircraft);
+    }
 
     // It should fill all the aircraft with ammo and subtract the needed ammo from the ammo storage
     // If there is not enough ammo then it should start to fill the aircrafts with priority first
     // If there is no ammo when this method is called, it should throw an exception.
-    public void fill(){}
+    public void fill(){
+
+    }
 
     // It should take another carrier as a reference parameter and fire all the ammo from the aircrafts to it,
     // then subtract all the damage from its health points
--- 09Week_Inheritance/src/I03_AircraftCarrier/I03_AircraftCarrier/Main.java
+++ 09Week_Inheritance/src/I03_AircraftCarrier/I03_AircraftCarrier/Main.java
@@ -2,14 +2,18 @@ package I03_AircraftCarrier;
 
 public class Main {
     public static void main(String[] args) {
-        Carrier carrier1 = new Carrier();
-        Carrier carrier2 = new Carrier();
+        Carrier carrier1 = new Carrier("carrier1");
+        Carrier carrier2 = new Carrier("carrier2");
 
         F16 sas = new F16();
         F35 tiger = new F35();
         F16 thunder = new F16();
         F35 starFighter = new F35();
 
+       carrier1.add(sas);
+       carrier1.add(tiger);
+       carrier2.add(thunder);
+       carrier2.add(starFighter);
 
 
     }
