6676d4ab073eb8a3d0768dac311c62971a532fe9
NemethJohanna <nemethkjohanna@gmail.com>
2020-01-06 21:31:45 +0100

    isinit finished

--- 05Week_Data_structures_Optional/src/D06_IsInList.java
+++ 05Week_Data_structures_Optional/src/D06_IsInList.java
@@ -3,10 +3,17 @@ import java.util.*;
 public class D06_IsInList {
     public static void main(String... args){
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
-        checkNums(list);
-        // Check if list contains all of the following elements: 4,8,12,16
-        // Create a method that accepts list as an input
-        // it should return "true" if it contains all, otherwise "false"
+        //checkNums(list);
+        System.out.println(checkNums(list));
+    }
 
+    private static boolean checkNums(ArrayList<Integer> list) {
+        boolean isTrue = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.contains(4) && list.contains(8) && list.contains(12) && list.contains(16)){
+                isTrue = true;
+            }
+        }
+        return isTrue;
     }
 }
