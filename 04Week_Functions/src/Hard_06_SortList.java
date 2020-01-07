97c542a8aabc639e98d52b1155bda034e994e5e5
NemethJohanna <nemethkjohanna@gmail.com>
2020-01-07 23:11:25 +0100

    started the sortList excercise

---
+++ 04Week_Functions/src/Hard_06_SortList.java
@@ -0,0 +1,28 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Hard_06_SortList {
+    public static void main(String[] args) {
+        //  Create a function that takes a list of numbers as parameter
+        //  Returns a list where the elements are sorted in ascending numerical order
+        //  Make a second boolean parameter, if it's `true` sort that list descending
+
+        //  Example:
+        int[] list = {34, 12, 24, 9, 5};
+        bubble(list);
+        //  should print [5, 9, 12, 24, 34]
+        advancedBubble(new int[]{34, 12, 24, 9, 5}, true);
+        //  should print [34, 24, 12, 9, 5]
+    }
+
+    public static void bubble(int[] list){
+        for (int i = 0; i < list.length; i++) {
+
+        }
+    }
+
+    public static void advancedBubble(int[] list, boolean trueOrFalse){
+
+    }
+
+}
