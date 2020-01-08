7972775d1e6e85063c06623953112158de707e88
NemethJohanna <nemethkjohanna@gmail.com>
2020-01-08 20:38:40 +0100

    finished reversed order

--- 07Week_File_Manipulation/src/F07_ReversedOrder.java
+++ 07Week_File_Manipulation/src/F07_ReversedOrder.java
@@ -2,27 +2,45 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class F07_ReversedOrder {
     // Create a method that decrypts reversed-order.txt
 
-    public static void main(String[] args) throws IOException {
-        Path path = Paths.get("reversed-order.txt");
-        List<String> order = new ArrayList();
-        try {
-            order = Files.readAllLines(path);
-            reverseOrder(order);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        Path filePath = Paths.get("reversed-order.txt");
+        reverseOrder(filePath);
     }
 
-    public static void reverseOrder (List<String> orderList) {
-        for (int i = orderList.size() - 1; i >= 0; i--) {
-            System.out.println(orderList.get(i));
+    private static void reverseOrder(Path filePath) {
+        try {
+            List<String> lines = Files.readAllLines(filePath);
+            for (int i = lines.size() - 1; i >= 0; i--) {
+                System.out.println(lines.get(i));
+            }
+            System.out.println();
+        } catch (IOException e){
+            System.out.println("Unable to read file.");
         }
-        System.out.println();
+
+
     }
+
+//    public static void main(String[] args) throws IOException {
+////        Path path = Paths.get("reversed-order.txt");
+////        List<String> order = new ArrayList();
+////        try {
+////            order = Files.readAllLines(path);
+////            reverseOrder(order);
+////        } catch (IOException e) {
+////            e.printStackTrace();
+////        }
+////    }
+////
+////    public static void reverseOrder (List<String> orderList) {
+////        for (int i = orderList.size() - 1; i >= 0; i--) {
+////            System.out.println(orderList.get(i));
+////        }
+////        System.out.println();
+////    }
 }
