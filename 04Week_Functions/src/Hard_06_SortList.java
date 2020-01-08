114c97c025e5ff4d8b75f93b26a10db7ac2b12b9
NemethJohanna <nemethkjohanna@gmail.com>
2020-01-08 18:46:30 +0100

    finished sortist excercise

--- 04Week_Functions/src/Hard_06_SortList.java
+++ 04Week_Functions/src/Hard_06_SortList.java
@@ -1,28 +1,35 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Hard_06_SortList {
     public static void main(String[] args) {
-        //  Create a function that takes a list of numbers as parameter
-        //  Returns a list where the elements are sorted in ascending numerical order
-        //  Make a second boolean parameter, if it's `true` sort that list descending
 
-        //  Example:
-        int[] list = {34, 12, 24, 9, 5};
+        List<Integer> list = new ArrayList<>();
+        list.add(34);
+        list.add(12);
+        list.add(24);
+        list.add(9);
+        list.add(5);
+
         bubble(list);
-        //  should print [5, 9, 12, 24, 34]
-        advancedBubble(new int[]{34, 12, 24, 9, 5}, true);
-        //  should print [34, 24, 12, 9, 5]
+        advancedBubble(list, true);
     }
 
-    public static void bubble(int[] list){
-        for (int i = 0; i < list.length; i++) {
-
+    public static void bubble(List<Integer> list){
+        for (int i = 0; i < list.size(); i++) {
+            Collections.sort(list);
         }
+        System.out.println(list);
     }
 
-    public static void advancedBubble(int[] list, boolean trueOrFalse){
-
+    public static void advancedBubble(List<Integer> list, boolean trueOrFalse){
+        if (trueOrFalse == true){
+            for (int i = 0; i < list.size(); i++) {
+                list.sort(Collections.reverseOrder());
+            }
+            System.out.println(list);
+        }
     }
 
 }
