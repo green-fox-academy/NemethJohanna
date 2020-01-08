f25c9b30f7533cc9bcea6a076f9ce5cad9630725
NemethJohanna <nemethkjohanna@gmail.com>
2020-01-08 19:58:23 +0100

    printEachLines javitva

--- 07Week_File_Manipulation/src/F01_PrintEachLine.java
+++ 07Week_File_Manipulation/src/F01_PrintEachLine.java
@@ -2,21 +2,19 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 
 public class F01_PrintEachLine {
     public static void main(String[] args) {
-        // Write a program that opens a file called "my-file.txt", then prints
-        // each line from the file.
-        // If the program is unable to read the file (for example it does not exist),
-        // then it should print the following error message: "Unable to read file: my-file.txt"
 
-        Path filePath = Paths.get("my-file.txt");
-        List<String> lines = new ArrayList<>();
         try {
-            lines = Files.readAllLines(filePath);
+            Path path = Paths.get("my-file.txt");
+            List<String> lines = Files.readAllLines(path);
+            for (int i = 0; i < lines.size(); i++) {
+                System.out.println(lines.get(i));
+            }
         } catch (IOException e) {
+            e.printStackTrace();
             System.out.println("Unable to read file: my-file.txt");
         }
 
