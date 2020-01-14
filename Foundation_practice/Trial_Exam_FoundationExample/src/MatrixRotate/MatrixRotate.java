9ba9bae28c511f6570b19e4b7e8d0f6ec2d9b299
NemethJohanna <nemethkjohanna@gmail.com>
2020-01-14 20:36:43 +0100

    finished matrix rotate excercise

--- Foundation_practice/Trial_Exam_FoundationExample/src/MatrixRotate/MatrixRotate.java
+++ Foundation_practice/Trial_Exam_FoundationExample/src/MatrixRotate/MatrixRotate.java
@@ -1,26 +1,28 @@
 package MatrixRotate;
 
+import java.util.Arrays;
+
 public class MatrixRotate {
     public static void main(String[] args) {
-        /*Create a function named rotateMatrix that takes an n×n integer matrix (array of arrays) as parameter and returns a matrix which elements are rotated 90 degrees clockwise.
 
-Example
-Input
+      int [][] matrix = { {1, 2, 3},
+                          {4, 5, 6},
+                          {7, 8, 9}, };
 
-[
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-]
-Output
-
-[ [ 7, 4, 1 ],
-  [ 8, 5, 2 ],
-  [ 9, 6, 3 ]
-]*/
-
-
-
+        rotateMatrix(matrix);
 
     }
+
+    public static void rotateMatrix(int[][] matrixToRotate){
+        int[][] matrix = new int[matrixToRotate.length][matrixToRotate.length];
+        for (int i = 0; i < matrixToRotate.length; i++) {
+            for (int j = 0; j < matrixToRotate[i].length; j++) {
+                matrix[i][(matrixToRotate.length - 1) - j] = matrixToRotate[j][i];
+            }
+            System.out.print(Arrays.toString(matrix[i]));
+            System.out.println();
+        }
+
+    }
+
 }
