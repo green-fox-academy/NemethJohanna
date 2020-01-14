be740817e528bc65a72d6a4f5bb96167221fdd15
NemethJohanna <nemethkjohanna@gmail.com>
2020-01-14 21:23:11 +0100

    finished swearWords excercise

--- Foundation_practice/Trial_Exam_FoundationExample/src/SwearWords/SwearWords.java
+++ Foundation_practice/Trial_Exam_FoundationExample/src/SwearWords/SwearWords.java
@@ -1,48 +1,46 @@
-//package SwearWords;
-//
-//import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class SwearWords {
-//    public static void main(String[] args) {
-///*Write a method which can read and parse a file containing not so family friendly text.
-//The method must remove all the given words from the file and return the amount of the removed words.
-//
-//Example
-//
-//Output
-//        //"fuck", "bloody", "cock", "shit", "fucker", "fuckstick", "asshole", "dick", "piss", "cunt"
-//17*/
-//
-//        Path filePath = Paths.get("reversed-lines.txt");
-//        List<String> lines = new ArrayList<>();
-//        try {
-//            lines = Files.readAllLines(filePath);
-//            removeWords(lines);
-//        } catch (IOException e) {
-//            System.out.println("Unable to read file: reversed-lines.txt");
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static int removeWords(List<String> line) {
-//        int nrOfWords = 0;
-//
-//        for (int i = 0; i < line.size(); i++) {
-//            for (int j = 0; j < line.get(i).length(); j--) {
-//                List<String> words = new ArrayList<String>();
-//                if (line.get(i)(j))
-//                line.remove();
-//
-//            }
-//            System.out.println();
-//        }
-//
-//        return nrOfWords;
-//    }
-//
-//}
+package SwearWords;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SwearWords {
+    public static void main(String[] args) {
+
+        String fileName = "C:\\Users\\Johanna Németh\\Desktop\\NemethJohanna\\Foundation_practice\\Trial_Exam_FoundationExample\\src\\SwearWords.txt";
+        System.out.println(removeWords(fileName));
+
+    }
+
+    public static int removeWords(String fileName) {
+        int nrOfWords = 0;
+        Path filePath = Paths.get(fileName);
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(filePath);
+        } catch (IOException e) {
+            System.out.println("Unable to read file: SwearWords.txt");
+            e.printStackTrace();
+        }
+
+        List<String> words = Arrays.asList("fuck", "bloody", "cock", "shit", "fucker", "fuckstick", "asshole", "dick", "piss", "cunt");
+
+        for (int i = 0; i < lines.size(); i++) {
+            String[] splitText = lines.get(i).split(" ");
+            for (int j = 0; j < splitText.length; j++) {
+                for (int k = 0; k < words.size(); k++) {
+                    if (splitText[j].equals(words.get(k))){
+                        lines.remove(splitText[j]);
+                        nrOfWords++;
+                    }
+                }
+            }
+        }
+        return nrOfWords;
+
+    }
+}
