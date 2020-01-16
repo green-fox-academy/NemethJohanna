12dd487d0b003ace11415dace7eacbfb986f505a
NemethJohanna <nemethkjohanna@gmail.com>
2020-01-16 15:50:50 +0100

    test added to greeter function

--- 04Week_Functions/src/F02_GreeterFunction.java
+++ 04Week_Functions/src/F02_GreeterFunction.java
@@ -1,21 +1,11 @@
-//  From now on, create the usual class wrapper
-//  and main method on your own.
-
-// - Create a string variable named `al` and assign the value `Green Fox` to it
-
-// - Create a function called `greet` that greets it's input parameter
-// - Greeting is printing e.g. `Greetings dear, Green Fox`
-// - Greet `al`
-
 public class F02_GreeterFunction {
     public static void main(String[] args) {
-        String al = "Green fox";
-        greet (al);
+        String al = "Johi";
+        System.out.println(greet(al));
     }
 
-    public static void greet (String name) {
-        System.out.println("Greetings dear, " + name + "!");
-
+    public static String greet (String name) {
+        return "Greetings dear, " + name + "!";
     }
 }
 
---
+++ 04Week_Functions/test/F02_GreeterFunctionTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class F02_GreeterFunctionTest {
+
+    @Test
+    public void greetingWorkingCorrectly() {
+        String result = "Greetings dear, Johi!";
+        assertEquals(result, F02_GreeterFunction.greet("Johi"));
+    }
+    
+}
