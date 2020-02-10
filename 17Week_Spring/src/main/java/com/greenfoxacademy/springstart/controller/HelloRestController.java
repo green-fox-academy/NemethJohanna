eb5b4f9d39897ed0111fcfbc73896b85c51c2ac7
NemethJohanna <nemethkjohanna@gmail.com>
2020-02-10 21:16:37 +0100

    Finished hello user rest app

--- 17Week_Spring/src/main/java/com/greenfoxacademy/springstart/controller/HelloRestController.java
+++ 17Week_Spring/src/main/java/com/greenfoxacademy/springstart/controller/HelloRestController.java
@@ -1,15 +1,17 @@
 package com.greenfoxacademy.springstart.controller;
 
 import com.greenfoxacademy.springstart.Greeting;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
 
     @GetMapping("/greeting")
-    public Greeting greeting(){
-        return new Greeting(1, "Hello World!");
+    public Greeting greeting(@RequestParam(value = "name", required = false) String name, Model model){
+        return new Greeting(1, "Hello " + name + "!");
     }
 
 }
