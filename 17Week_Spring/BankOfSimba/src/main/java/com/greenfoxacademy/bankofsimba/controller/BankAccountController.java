8a3405b31b7746200204b327341e31a19abec925
NemethJohanna <nemethkjohanna@gmail.com>
2020-02-14 10:47:13 +0100

    formatted some tiny thigs

--- 17Week_Spring/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
+++ 17Week_Spring/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BankAccountController {
 
+    BankAccount simbaBank = new BankAccount("Simba", 2000, "lion");
+
     @GetMapping(path = "/account")
     public String showAccount(Model model, String name, Integer balance, String type) {
-        model.addAttribute("name", "Simba");
-        model.addAttribute("balance", 2000);
-        model.addAttribute("type", "lion");
+        model.addAttribute("name", simbaBank.getName());
+        model.addAttribute("balance", simbaBank.getBalance());
+        model.addAttribute("type", simbaBank.getAnimalType());
         return "bank";
     }
 
