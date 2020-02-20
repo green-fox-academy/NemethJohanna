7211b4a70b15ad27b9d22252d369c77198abec2e
NemethJohanna <nemethkjohanna@gmail.com>
2020-02-20 12:06:10 +0100

    finished homePage and random background color

--- 18Week_Dependencies/UsefulUtilities/src/main/java/com/greenfoxacademy/usefulutilities/controller/UtilityController.java
+++ 18Week_Dependencies/UsefulUtilities/src/main/java/com/greenfoxacademy/usefulutilities/controller/UtilityController.java
@@ -1,16 +1,37 @@
 package com.greenfoxacademy.usefulutilities.controller;
 
+import com.greenfoxacademy.usefulutilities.service.UtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UtilityController {
 
-    @Autowired
-    @GetMapping("/useful")
-    public String details (){
+    UtilityService service;
 
-        return "useful";
+    @Autowired
+    public UtilityController(UtilityService service) {
+        this.service = service;
     }
+
+    @GetMapping(path = "/useful")
+    public String details() {
+        return "links";
+    }
+
+    @GetMapping(path = "/useful/background")
+    public String coloredBackground (Model model){
+        model.addAttribute("backgroundColor", service.randomColor());
+        return "background";
+    }
+
+//    @GetMapping(path = "/useful/email")
+//    public String emailValidator(){
+//
+//        return "email";
+//    }
+
+
 }
