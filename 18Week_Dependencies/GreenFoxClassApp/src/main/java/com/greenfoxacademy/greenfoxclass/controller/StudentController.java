ee56a55a1a55d0d5f21a752349d32cef08af06e1
NemethJohanna <nemethkjohanna@gmail.com>
2020-02-21 11:35:14 +0100

    started the greenfox classes eercise, adding new student not working

---
+++ 18Week_Dependencies/GreenFoxClassApp/src/main/java/com/greenfoxacademy/greenfoxclass/controller/StudentController.java
@@ -0,0 +1,44 @@
+package com.greenfoxacademy.greenfoxclass.controller;
+
+import com.greenfoxacademy.greenfoxclass.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    private StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping(path = "/gfa")
+    public String main() {
+        return "index";
+    }
+
+    @GetMapping(path = "/gfa/list")
+    public String listStudents(Model model) {
+        model.addAttribute("list", studentService.findAll());
+        return "list";
+    }
+
+    @GetMapping(path = "gfa/add")
+    public String addStudent(@ModelAttribute String name, Model model) {
+        model.addAttribute("student", name);
+        return "addnewstudent";
+    }
+
+    @PostMapping(path = "/gfa/add")
+    public String addToTheList(String studentsName) {
+        studentService.save(studentsName);
+        return "redirect:/list";
+    }
+
+}
---
+++ 18Week_Dependencies/GreenFoxClassApp/src/test/java/com/greenfoxacademy/greenfoxclass/GreenfoxclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.greenfoxclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenfoxclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
