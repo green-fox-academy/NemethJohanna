5b709b7dd2da61232d90db78aadd2a59d97f43fb
NemethJohanna <nemethkjohanna@gmail.com>
2020-02-24 11:01:47 +0100

    login page, and index page already working, created the fox.class

--- 19Week_ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/controller/MainController.java
+++ 19Week_ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/controller/MainController.java
@@ -1,28 +1,29 @@
 package com.greenfoxacademy.programmerfoxclub.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping(path = "/")
-    public String index() {
+    public String index(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
         return "index";
     }
 
     @GetMapping(path = "/login")
     public String showLogin() {
-
         return "login";
     }
 
-
     @PostMapping(path = "/login")
-    public String loginFox() {
-
-        return "redirect:/";
+    public String loginFox(@RequestParam (value = "name") String name) {
+        return "redirect:/?name=" + name;
     }
 
 }
