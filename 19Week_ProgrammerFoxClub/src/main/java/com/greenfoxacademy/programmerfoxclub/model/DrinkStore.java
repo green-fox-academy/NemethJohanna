86cebe5d5b596b7c73f3f00305ffb1f282a40be0
NemethJohanna <nemethkjohanna@gmail.com>
2020-02-24 11:43:58 +0100

    created the food and drink store models

---
+++ 19Week_ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/model/DrinkStore.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.programmerfoxclub.model;
+
+import java.util.List;
+
+public class DrinkStore {
+
+    private List<String> drink;
+
+    public DrinkStore(){
+        drink.add("water");
+        drink.add("milk");
+        drink.add("wine");
+    }
+
+}
---
+++ 19Week_ProgrammerFoxClub/src/main/java/com/greenfoxacademy/programmerfoxclub/model/FoodStore.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.programmerfoxclub.model;
+
+import java.util.List;
+
+public class FoodStore {
+
+    private List<String> food;
+
+    public FoodStore(){
+        food.add("meat");
+        food.add("grass");
+        food.add("seeds");
+    }
+
+}
