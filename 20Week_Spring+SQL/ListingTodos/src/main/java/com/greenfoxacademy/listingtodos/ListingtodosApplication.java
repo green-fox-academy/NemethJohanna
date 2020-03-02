f56bd6479b4ee94f8ec88d080de6e24f66fda775
NemethJohanna <nemethkjohanna@gmail.com>
2020-03-02 22:39:54 +0100

    implemented the command Line runner

--- 20Week_Spring+SQL/ListingTodos/src/main/java/com/greenfoxacademy/listingtodos/ListingtodosApplication.java
+++ 20Week_Spring+SQL/ListingTodos/src/main/java/com/greenfoxacademy/listingtodos/ListingtodosApplication.java
@@ -1,13 +1,18 @@
 package com.greenfoxacademy.listingtodos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ListingtodosApplication {
+public class ListingtodosApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ListingtodosApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
 }
