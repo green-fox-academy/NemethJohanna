7775e27914dea22c1126e73ca5a63c2d57046e32
NemethJohanna <nemethkjohanna@gmail.com>
2020-03-02 22:16:52 +0100

    created the packages and the classes

---
+++ 20Week_Spring+SQL/ListingTodos/src/main/java/com/greenfoxacademy/listingtodos/model/Todo.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.listingtodos.model;
+
+public class Todo {
+
+    private long id;
+    private String title;
+    private boolean urgent;
+    private boolean done;
+
+}
