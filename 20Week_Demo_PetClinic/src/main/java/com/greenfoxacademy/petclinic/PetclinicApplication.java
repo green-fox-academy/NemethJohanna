21f613445a0a9b688a5749eed03446523e0f054b
NemethJohanna <nemethkjohanna@gmail.com>
2020-03-04 23:21:43 +0100

    add html page is visible, most of the buttons working well

--- 20Week_Demo_PetClinic/src/main/java/com/greenfoxacademy/petclinic/PetclinicApplication.java
+++ 20Week_Demo_PetClinic/src/main/java/com/greenfoxacademy/petclinic/PetclinicApplication.java
@@ -1,7 +1,7 @@
 package com.greenfoxacademy.petclinic;
 
 import com.greenfoxacademy.petclinic.models.Owner;
-//import com.greenfoxacademy.petclinic.models.Pet;
+import com.greenfoxacademy.petclinic.models.Pet;
 import com.greenfoxacademy.petclinic.repositories.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,9 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetclinicApplication
-//        implements CommandLineRunner
-{
+public class PetclinicApplication {
 
 //    private OwnerRepository ownerRepository;
 //
@@ -26,7 +24,9 @@ public class PetclinicApplication
 
 //    @Override
 //    public void run(String... args) throws Exception {
-//        ownerRepository.save(new Owner("Jozsi", "14. Petofi Street", "Budapest", "+36204587264", (new Pet("dog", "Marci", "02/2013"))));
+//        ownerRepository.save(new Owner("Jozsi", "14. Petofi Street", "Budapest", "+36204587264"
+//                , (new Pet("dog", "Marci", "02/2018"))
+//        ));
 //        ownerRepository.save(new Owner("Anna", "10. Mango Street", "Szentendre", "+36201235847", (new Pet("cat", "Cirmi", "08/2016"))));
 //        ownerRepository.save(new Owner("Petra", "8. Apple Street", "Budakeszi", "+36301234567", (new Pet("snake", "Kiki", "01/2020"))));
 //    }
--- 20Week_Demo_PetClinic/src/main/java/com/greenfoxacademy/petclinic/controllers/MainController.java
+++ 20Week_Demo_PetClinic/src/main/java/com/greenfoxacademy/petclinic/controllers/MainController.java
@@ -1,38 +1,59 @@
-//package com.greenfoxacademy.petclinic.controllers;
-//
-//import com.greenfoxacademy.petclinic.services.OwnerService;
-//import com.greenfoxacademy.petclinic.services.PetService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/petclinic")
-//public class MainController {
-//
-////    private OwnerService ownerService;
-////    private PetService petService;
-//
-////    @Autowired
-////    public MainController(OwnerService ownerService, PetService petService) {
-////        this.ownerService = ownerService;
-////        this.petService = petService;
-////    }
-//
-//    @GetMapping(path = {"/", ""})
-//    public String welcome(){
-//        return "index";
-//    }
-//
-//    @GetMapping(path = "/owners")
-//    public String listOwners(Model model){
-//        model.addAttribute("owners", ownerService.list());
+package com.greenfoxacademy.petclinic.controllers;
+
+import com.greenfoxacademy.petclinic.services.OwnerService;
+import com.greenfoxacademy.petclinic.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/petclinic")
+public class MainController {
+
+    private OwnerService ownerService;
+    private PetService petService;
+
+    @Autowired
+    public MainController(OwnerService ownerService, PetService petService) {
+        this.ownerService = ownerService;
+        this.petService = petService;
+    }
+
+    @GetMapping(path = {"/", ""})
+    public String welcome() {
+        return "welcome";
+    }
+
+    @GetMapping(path = "/findowner")
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.list());
 //        model.addAttribute("pets", petService.list());
-//        return "listOwners";
-//    }
-//
-//
-//
-//}
+        return "findowner";
+    }
+
+    @GetMapping(path = "/listowner")
+    public String list (Model model){
+        model.addAttribute("owners", ownerService.list());
+        return "listowner";
+    }
+
+    @GetMapping(path = "/addowner")
+    public String add(){
+        return "addowner";
+    }
+
+
+    @GetMapping(path = "/veterinarians")
+    public String veter() {
+        return "veterinarians";
+    }
+
+    @GetMapping(path = "/error")
+    public String error() {
+        return "error";
+    }
+
+
+}
