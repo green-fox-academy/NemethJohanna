c1579ae239d463fe10d75a8319675f4f636d21ea
NemethJohanna <nemethkjohanna@gmail.com>
2020-03-09 18:57:57 +0100

    changed the name of the folder

---
+++ 20Week_Spring+SQL/MySql/src/main/java/com/greenfoxacademy/mysql/repositories/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy.mysql.repositories;
+
+import com.greenfoxacademy.mysql.models.Todo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+    Iterable<Todo> findAllByIsDone(Boolean isDone);
+}
