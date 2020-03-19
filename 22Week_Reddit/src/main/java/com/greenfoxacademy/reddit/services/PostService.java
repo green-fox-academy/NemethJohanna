9380a74e96f9d6a4b7d92a2c35a34da953369e25
NemethJohanna <nemethkjohanna@gmail.com>
2020-03-19 15:51:42 +0100

    deleted the unused lines, and the decreasing can go under zero

--- 22Week_Reddit/src/main/java/com/greenfoxacademy/reddit/services/PostService.java
+++ 22Week_Reddit/src/main/java/com/greenfoxacademy/reddit/services/PostService.java
@@ -2,7 +2,6 @@ package com.greenfoxacademy.reddit.services;
 
 import com.greenfoxacademy.reddit.models.Post;
 import com.greenfoxacademy.reddit.repositories.PostRepository;
-import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,17 +17,17 @@ public class PostService {
         this.postRepository = postRepository;
     }
 
-    public Iterable<Post> list(){
+    public Iterable<Post> list() {
         return postRepository.findAll();
     }
 
-    public void add(Post post){
+    public void add(Post post) {
         postRepository.save(post);
     }
 
-    public void raiseScore(int id){
+    public void raiseScore(int id) {
         Optional<Post> post = postRepository.findById(id);
-        if (post.isPresent()){
+        if (post.isPresent()) {
             Post postScoreRaising = post.get();
             postScoreRaising.setScore(postScoreRaising.getScore() + 1);
             postRepository.save(postScoreRaising);
@@ -36,14 +35,12 @@ public class PostService {
 
     }
 
-    public void decreaseScore(int id){
+    public void decreaseScore(int id) {
         Optional<Post> post = postRepository.findById(id);
-        if (post.isPresent()){
+        if (post.isPresent()) {
             Post postScoreDecreasing = post.get();
-            if (postScoreDecreasing.getScore() > 0){
-                postScoreDecreasing.setScore(postScoreDecreasing.getScore() - 1);
-                postRepository.save(postScoreDecreasing);
-            }
+            postScoreDecreasing.setScore(postScoreDecreasing.getScore() - 1);
+            postRepository.save(postScoreDecreasing);
         }
     }
 
