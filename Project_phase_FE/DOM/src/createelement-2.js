9f3d83981f1d3a179e99ce358610f162498c4f25
NemethJohanna <nemethkjohanna@gmail.com>
2020-05-05 23:01:25 +0200

    started the createElement-2, but not finished the most

---
+++ Project_phase_FE/DOM/src/createelement-2.js
@@ -0,0 +1,10 @@
+// Remove the king from the list.
+let asteroids = document.getElementsByClassName("asteroids")[0];
+let king = asteroids.getElementsByTagName("li")[0];
+asteroids.removeChild(king);
+
+//     Fill the list based on the following list of objects.
+
+//     Only add the asteroids to the list.
+
+//     Each list item should have its category as a class and its content as text content.
