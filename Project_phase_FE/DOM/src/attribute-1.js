3ad071614129d8832ddf56b64749c4d3434d4f12
NemethJohanna <nemethkjohanna@gmail.com>
2020-05-05 21:57:21 +0200

    attribute-1 finished

---
+++ Project_phase_FE/DOM/src/attribute-1.js
@@ -0,0 +1,15 @@
+// Write the image's url to the console.
+// Replace the image with a picture of your favorite animal.
+let img = document.getElementsByTagName("img")[0];
+img.setAttribute("src", "https://birdwatchinghq.com/hedgehog-facts/");
+
+//     Make the link point to the Green Fox Academy website.
+let link = document.getElementsByTagName("a")[0];
+link.setAttribute("href", "https://www.greenfoxacademy.com/");
+
+//     Disable the second button.
+
+
+//     Replace its text with 'Don't click me!'.
+let secondButton = document.getElementsByTagName("button")[1];
+secondButton.innerHTML = "Don't click me!";
