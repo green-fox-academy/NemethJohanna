0b67c7ae5de57b255e3a386f5b95c5fba2744095
NemethJohanna <nemethkjohanna@gmail.com>
2020-05-05 21:13:10 +0200

    finished the firdóst part of content-1

--- Project_phase_FE/DOM/src/content-1.js
+++ Project_phase_FE/DOM/src/content-1.js
@@ -1,8 +1,14 @@
 // 1)  Fill every paragraph with the last one's content.(innerHTML)
-var lastOne = document.querySelector('.animals');
-var apple = document.querySelector('.apple');
-apple.innerHTML = lastOne ;
-console.log(lastOne);
-console.log(apple);
+let apple = document.getElementsByClassName("apple")[0];
+let balloon = document.getElementsByClassName("balloon")[0];
+let cat = document.getElementsByClassName("cat")[0];
+let animal = document.getElementsByClassName("animals")[0];
+
+let getContent = animal.innerHTML; //goat rabbit <strong>cat</strong> dog
+
+apple.innerHTML = getContent;
+balloon.innerHTML = getContent;
+cat.innerHTML = getContent;
 
 // 2)  Do the same again, but you should keep the cat strong.
+
